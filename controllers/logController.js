a315e485c844f08f2f6214d92b27dd7a1847adeb
Michael White <michaelwhite404@gmail.com>
2021-08-01 22:26:16 -0400

    Deleted js files replaced by ts integration

--- controllers/logController.js
+++
@@ -1,9 +0,0 @@
-const CheckoutLog = require("../models/checkoutLogModel")
-const factory = require("./handlerFactory");
-
-exports.getAllLogs = factory.getAll(CheckoutLog)
-exports.getLog = factory.getOne(CheckoutLog, { 
-  path: 'device deviceUser teacherCheckOut teacherCheckIn', 
-  select: 'deviceType name fullName grade email' 
-});
-exports.getLogsByDevice = factory.getAll(CheckoutLog);
