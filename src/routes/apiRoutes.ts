cb0de183e1126505a94a9110e7b19db161cc71e1
Michael White <michaelwhite404@gmail.com>
2021-08-03 22:28:15 -0400

    Added v2 routes

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import v1Router from "./v1/v1Routes";
+import v2Router from "./v2/v2Routes";
 
 const router = Router();
 
 router.use("/v1", v1Router);
-
-// router.use(["/v2", "/"])
+router.use(["/v2", "/"], v2Router);
 
 export default router;
---
+++ src/routes/v2/v2Routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const v2Router = Router();
+
+// v2Router.use("/students" /* ,studentRouter */);
+// v2Router.use("/users" /* , userRouter */);
+// v2Router.use("/devices" /* , deviceRouter */);
+// v2Router.use("/textbook-sets"/* , textbookSetRouter */);
+// v2Router.use("/employee-leave" /*, employeeLeaveRouter */);
+// v2Router.use("/reimbursement" /* , reimbursementRouter */);
+// v2Router.use("/ideas" /* ,ideaRouter */);
+
+export default v2Router;
