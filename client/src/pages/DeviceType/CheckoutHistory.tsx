93b5e30c4d855b6e0491cc3a7388405cf929e849
Michael White <michaelwhite404@gmail.com>
2021-12-22 06:53:20 -0500

    Fetch device checkout logs

--- client/src/pages/DeviceType/CheckoutHistory.tsx
+++ client/src/pages/DeviceType/CheckoutHistory.tsx
@@ -1,13 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { DeviceModel } from "../../../../src/types/models/deviceTypes";
 import { CheckoutLogModel } from "../../../../src/types/models/checkoutLogTypes";
 import PaneHeader from "../../components/PaneHeader/PaneHeader";
 import axios, { AxiosError } from "axios";
 import { APICheckoutLogResponse, APIError } from "../../types/apiResponses";
+import TablePaginate from "../../components/TablePaginate/TablePaginate";
+import Badge from "../../components/Badge/Badge";
+import BadgeColor from "../../components/Badge/BadgeColor";
 
 interface CheckoutHistoryProps {
   device: DeviceModel;
 }
+
+const statusColor: { [x: string]: BadgeColor } = {
+  "Checked In": "emerald",
+  "Checked Out": "red",
+  "Checked In /w Error": "yellow",
+};
 
 export default function CheckoutHistory({ device }: CheckoutHistoryProps) {
   const [checkouts, setCheckouts] = useState<CheckoutLogModel[]>([]);
@@ -18,7 +27,7 @@ export default function CheckoutHistory({ device }: CheckoutHistoryProps) {
     async function getCheckouts() {
       try {
         const res = await axios.get<APICheckoutLogResponse>("/api/v2/devices/logs", {
-          params: { device: device._id },
+          params: { device: device._id, sort: "-checkOutDate" },
         });
         setCheckouts(res.data.data.deviceLogs);
       } catch (err) {
@@ -27,18 +36,47 @@ export default function CheckoutHistory({ device }: CheckoutHistoryProps) {
     }
   }, [device._id]);
 
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Status",
+        accessor: "checkedIn",
+        Cell: ({ row: { original } }: { row: { original: CheckoutLogModel } }) => {
+          let text = "";
+          original.checkedIn
+            ? original.error
+              ? (text = "Checked In /w Error")
+              : (text = "Checked In")
+            : (text = "Checked Out");
+          return <Badge color={statusColor[text]} text={text} />;
+        },
+      },
+      {
+        Header: "Student",
+        accessor: "deviceUser.fullName",
+      },
+      {
+        Header: "Check Out",
+        accessor: "checkOutDate",
+        Cell: ({ row: { original } }: { row: { original: CheckoutLogModel } }) => {
+          return new Date(original.checkOutDate).toLocaleString();
+        },
+      },
+      {
+        Header: "Check In",
+        accessor: "checkInDate",
+        Cell: ({ row: { original } }: { row: { original: CheckoutLogModel } }) => {
+          return original.checkInDate ? new Date(original.checkInDate).toLocaleString() : "-";
+        },
+      },
+    ],
+    []
+  );
+
   return (
     <div>
       <PaneHeader>Check Out History</PaneHeader>
-      {checkouts.map((log) => (
-        <div>
-          <div>{log.checkedIn ? "true" : "false"}</div>
-          <div>{log.deviceUser}</div>
-          <div>{log.checkInDate}</div>
-          <div>{log.checkOutDate}</div>
-          <hr />
-        </div>
-      ))}
+      <TablePaginate data={checkouts} columns={columns} pageSize={20} />
     </div>
   );
 }
--- src/controllers/v2/deviceLogController.ts
+++ src/controllers/v2/deviceLogController.ts
@@ -1,14 +1,16 @@
 import { RequestHandler } from "express";
 import CheckoutLog from "../../models/checkoutLogModel";
+import PopOptions from "../../types/popOptions";
 import * as factory from "./handlerFactory";
 
 const Model = CheckoutLog;
 const key = "deviceLog";
+const pop = { path: "deviceUser teacherCheckOut teacherCheckIn", select: "fullName" } as PopOptions;
 
 /** `GET` - Gets all device logs
  *  - All authorized users can access this route
  */
-export const getAllLogs: RequestHandler = factory.getAll(Model, `${key}s`);
+export const getAllLogs: RequestHandler = factory.getAll(Model, `${key}s`, {}, pop);
 
 /** `GET` - Gets a single device log
  *  - All authorized users can access this route
