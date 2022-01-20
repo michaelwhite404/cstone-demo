88d568f3cd2393bb2d69e5deb7805e0178d6d6c6
Michael White <michaelwhite404@gmail.com>
2022-01-19 20:35:24 -0500

    Responsive basic info section

--- client/src/pages/DeviceType2/BasicInfo/BasicInfoCard.tsx
+++ client/src/pages/DeviceType2/BasicInfo/BasicInfoCard.tsx
@@ -5,10 +5,12 @@ interface CardDataProps {
 }
 
 export default function BasicInfoCard({ data }: { data: CardDataProps }) {
-  return (
+  const Card = (
     <div className="basic-info-card">
       <div className="basic-info-card-heading">{data.heading}</div>
       <div className="basic-info-card-value">{data.value}</div>
     </div>
   );
+
+  return data.value ? Card : <></>;
 }
