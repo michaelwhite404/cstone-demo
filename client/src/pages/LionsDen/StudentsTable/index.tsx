d171827e3cd5a70a481afec7b59170e8e1bff82d
Michael White <michaelwhite404@gmail.com>
2022-05-01 17:16:52 -0400

    Added main checkbox functionality

--- client/src/pages/LionsDen/StudentsTable/index.tsx
+++ client/src/pages/LionsDen/StudentsTable/index.tsx
@@ -1,17 +1,62 @@
 import { Checkbox } from "@mui/material";
+import { useEffect, useState } from "react";
 import { StudentModel } from "../../../../../src/types/models";
 import TableWrapper from "../../../components/TableWrapper";
 import { numberToGrade } from "../../../utils/grades";
 import "./StudentsTable.sass";
 
+type RemoveArray<T> = T extends any[] ? T[number] : never;
+type CheckboxStates = "unchecked" | "checked" | "indeterminate";
+
 export default function StudentTable({ students }: { students: StudentAftercareStat[] }) {
+  const [rows, setRows] = useState(students.map((s) => ({ ...s, checked: false })));
+  const [main, setMain] = useState<CheckboxStates>("unchecked");
+
+  const mainCheckboxClick = () => {
+    if (main === "checked") {
+      switchAll(false);
+      return setMain("unchecked");
+    }
+    switchAll(true);
+    setMain("checked");
+  };
   const headings = [
-    { comp: <Checkbox />, className: "sm:w-auto w-20" },
+    {
+      comp: (
+        <Checkbox
+          onClick={mainCheckboxClick}
+          indeterminate={main === "indeterminate"}
+          checked={main === "checked"}
+        />
+      ),
+      className: "sm:w-auto w-20",
+    },
     { comp: "Student", className: "" },
     { comp: "Grade", className: "hidden sm:table-cell" },
     { comp: "Days Present", className: "hidden sm:table-cell" },
     { comp: "Days Late", className: "hidden sm:table-cell" },
   ];
+
+  useEffect(() => {
+    setRows(students.map((s) => ({ ...s, checked: false })));
+  }, [students]);
+
+  useEffect(() => {
+    console.log("HERE");
+    const numChecked = rows.filter((r) => r.checked).length;
+    if (numChecked === 0) return setMain("unchecked");
+    if (numChecked === rows.length) return setMain("checked");
+    setMain("indeterminate");
+  }, [rows]);
+
+  const onCheckboxChange = (row: RemoveArray<typeof rows>) => {
+    const copiedRows = [...rows];
+    const findIndex = copiedRows.findIndex((r) => r._id === row._id);
+    rows[findIndex].checked = !rows[findIndex].checked;
+    setRows(copiedRows);
+  };
+
+  const switchAll = (checked: boolean) => setRows([...rows].map((row) => ({ ...row, checked })));
 
   return (
     <TableWrapper>
@@ -24,10 +69,10 @@ export default function StudentTable({ students }: { students: StudentAftercareS
           </tr>
         </thead>
         <tbody>
-          {students.map((student) => (
+          {rows.map((student) => (
             <tr style={{ borderBottom: "1px #e5e7eb solid" }} key={student._id}>
               <td>
-                <Checkbox />
+                <Checkbox checked={student.checked} onChange={() => onCheckboxChange(student)} />
               </td>
               <td>
                 {student.fullName}
