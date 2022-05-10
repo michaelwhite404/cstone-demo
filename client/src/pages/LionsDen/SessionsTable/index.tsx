4fd787e820d09e6645fb7be330f047d420b27f48
Michael White <michaelwhite404@gmail.com>
2022-05-09 21:40:32 -0400

    Added sections for dropIns on SessionsTable

--- client/src/pages/LionsDen/SessionsTable/index.tsx
+++ client/src/pages/LionsDen/SessionsTable/index.tsx
@@ -5,6 +5,14 @@ import { SignedOutEntry } from "../../../types/aftercareTypes";
 import "./SessionsTable.sass";
 
 export default function SessionsTable({ entries }: { entries: SignedOutEntry[] }) {
+  const regular: SignedOutEntry[] = [];
+  const dropIns: SignedOutEntry[] = [];
+
+  entries.forEach((entry) => {
+    const array = !entry.dropIn ? regular : dropIns;
+    array.push(entry);
+  });
+
   return (
     <TableWrapper>
       <table className="aftercare-session-table">
@@ -17,38 +25,48 @@ export default function SessionsTable({ entries }: { entries: SignedOutEntry[] }
           </tr>
         </thead>
         <tbody>
-          {entries.map((entry) => (
-            <tr style={{ borderBottom: "1px #e5e7eb solid" }} key={entry._id}>
-              <td>
-                <div>{entry.student.fullName}</div>
-                <div className="show-at-640">
-                  <div
-                    style={{
-                      color: "gray",
-                      fontSize: 10,
-                      margin: "7px 0",
-                    }}
-                  >
-                    Time: {format(new Date(entry.signOutDate), "h:mm aa")}
-                  </div>
-                  {entry.lateSignOut && <Badge text="Late" color="red" />}
-                </div>
-              </td>
-              <td className="hide-at-640">{format(new Date(entry.signOutDate), "h:mm aa")}</td>
-              <td>
-                <img
-                  className="sign-out-signature"
-                  src={`/images/${entry.signature}`}
-                  alt="signature"
-                />
-              </td>
-              <td className="hide-at-640 late-td">
-                {entry.lateSignOut && <Badge text="Late" color="red" />}
-              </td>
-            </tr>
+          {regular.map((entry) => (
+            <EntryRow key={entry._id} entry={entry} />
+          ))}
+          <tr>
+            <td className="table-header" style={{ height: "auto", padding: 15 }} colSpan={4}>
+              Drop Ins
+            </td>
+          </tr>
+          {dropIns.map((entry) => (
+            <EntryRow key={entry._id} entry={entry} />
           ))}
         </tbody>
       </table>
     </TableWrapper>
+  );
+}
+
+function EntryRow({ entry }: { entry: SignedOutEntry }) {
+  return (
+    <tr style={{ borderBottom: "1px #e5e7eb solid" }} key={entry._id}>
+      <td>
+        <div>{entry.student.fullName}</div>
+        <div className="show-at-640">
+          <div
+            style={{
+              color: "gray",
+              fontSize: 10,
+              margin: "7px 0",
+            }}
+          >
+            Time: {format(new Date(entry.signOutDate), "h:mm aa")}
+          </div>
+          {entry.lateSignOut && <Badge text="Late" color="red" />}
+        </div>
+      </td>
+      <td className="hide-at-640">{format(new Date(entry.signOutDate), "h:mm aa")}</td>
+      <td>
+        <img className="sign-out-signature" src={`/images/${entry.signature}`} alt="signature" />
+      </td>
+      <td className="hide-at-640 late-td">
+        {entry.lateSignOut && <Badge text="Late" color="red" />}
+      </td>
+    </tr>
   );
 }
