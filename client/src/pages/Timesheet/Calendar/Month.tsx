7ed5a1c4b2478d54d7ddc94d3e04fead3dd66483
Michael White <michaelwhite404@gmail.com>
2022-07-07 10:40:50 -0400

    Added day button click handler

--- client/src/pages/Timesheet/Calendar/Month.tsx
+++ client/src/pages/Timesheet/Calendar/Month.tsx
@@ -11,15 +11,28 @@ import {
 import classNames from "classnames";
 import Month from "../../../types/month";
 import CalendarEvent from "../../../types/calendarEvent";
+import { ClockIcon } from "@heroicons/react/solid";
 
 const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 export function CalendarMonth(props: CalendarMonthProps) {
   const [days, setDays] = useState(createDates(props.month, props.year, props.events));
+  const selectedDay = days.find((day) => day.isSelected);
 
   useEffect(() => {
     setDays(createDates(props.month, props.year, props.events));
   }, [props.events, props.month, props.year]);
+
+  const onDayButtonClick = (date: string) => {
+    const daysClone = [...days];
+    const selectedDayIndex = daysClone.findIndex((day) => day.isSelected);
+    selectedDayIndex > -1 && (daysClone[selectedDayIndex].isSelected = false);
+    const dayIndex = daysClone.findIndex((day) => day.date === date);
+    if (dayIndex > -1) {
+      daysClone[dayIndex].isSelected = true;
+    }
+    setDays(daysClone);
+  };
 
   return (
     <div>
@@ -97,6 +110,7 @@ export function CalendarMonth(props: CalendarMonthProps) {
                   !day.isSelected && !day.isCurrentMonth && !day.isToday && "text-gray-500",
                   "flex h-14 flex-col py-2 px-3 hover:bg-gray-100 focus:z-10"
                 )}
+                onClick={() => onDayButtonClick(day.date)}
               >
                 <time
                   dateTime={day.date}
@@ -125,6 +139,34 @@ export function CalendarMonth(props: CalendarMonthProps) {
           </div>
         </div>
       </div>
+      {selectedDay?.events && selectedDay?.events.length > 0 && (
+        <div className="py-10 px-4 sm:px-6 lg:hidden">
+          <ol className="divide-y divide-gray-100 overflow-hidden rounded-lg bg-white text-sm shadow ring-1 ring-black ring-opacity-5">
+            {selectedDay.events.map((event) => (
+              <li
+                key={event.id}
+                className="group flex p-4 pr-6 focus-within:bg-gray-50 hover:bg-gray-50"
+              >
+                <div className="flex-auto">
+                  <p className="font-semibold text-gray-900">{event.description}</p>
+                  <time
+                    /* dateTime={event.datetime} */ className="mt-2 flex items-center text-gray-700"
+                  >
+                    <ClockIcon className="mr-2 h-5 w-5 text-gray-400" aria-hidden="true" />
+                    {event.timeLabel}
+                  </time>
+                </div>
+                <a
+                  href={"#"}
+                  className="ml-6 flex-none self-center rounded-md border border-gray-300 bg-white py-2 px-3 font-semibold text-gray-700 opacity-0 shadow-sm hover:bg-gray-50 focus:opacity-100 group-hover:opacity-100"
+                >
+                  Edit<span className="sr-only">, {event.description}</span>
+                </a>
+              </li>
+            ))}
+          </ol>
+        </div>
+      )}
     </div>
   );
 }
