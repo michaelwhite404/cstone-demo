386076f2d482324d905d486092b7bd2908267fbd
Michael White <michaelwhite404@gmail.com>
2022-07-09 16:30:59 -0400

    Created LabeledInput2

---
+++ client/src/components/LabeledInput2.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function LabeledInput2(props: LabeledInput2Props) {
+  const { placeholder, id, name, type, ...rest } = props;
+
+  return (
+    <>
+      <label htmlFor={name} className="block text-sm font-medium text-gray-700">
+        {props.label}
+      </label>
+      <div className="mt-1">
+        <input
+          type={type}
+          name={name}
+          id={id}
+          className="py-2 px-3 shadow focus:border-blue-500 border-white border-2 block w-full sm:text-sm rounded-md"
+          placeholder={placeholder}
+          style={{ boxShadow: "0px 0px 2px #aeaeae" }}
+          {...rest}
+        />
+      </div>
+    </>
+  );
+}
+
+interface LabeledInput2Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  type?: "email" | "input";
+}
