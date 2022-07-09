0f1ce55e35cdb04a4c0e2a4bed9dec0d56013e3a
Michael White <michaelwhite404@gmail.com>
2022-07-08 23:02:09 -0400

    Close calendar view dropdown on click

--- client/src/pages/Timesheet/Calendar/ViewDropdown.tsx
+++ client/src/pages/Timesheet/Calendar/ViewDropdown.tsx
@@ -1,25 +1,21 @@
 import { Menu } from "@blueprintjs/core";
-import { Popover2 } from "@blueprintjs/popover2";
+import { Classes, Popover2 } from "@blueprintjs/popover2";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 import capitalize from "capitalize";
-import { useToggle } from "../../../hooks";
 import { CalendarView } from "../../../types/calendar";
 
 const views: CalendarView[] = ["day", "week", "month", "year"];
 
 export function CalendarViewDropdown(props: ViewProps) {
-  const [open, toggle] = useToggle(false);
   return (
     <Popover2
-      content={<CalendarViewMenu {...props} close={toggle} />}
+      content={<CalendarViewMenu {...props} />}
       placement="bottom-end"
       className="menu-popover"
-      isOpen={open}
+      interactionKind="click"
+      hoverCloseDelay={10000}
     >
-      <button
-        className="flex items-center rounded-md border border-gray-300 bg-white py-2 pl-3 pr-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50"
-        onClick={toggle}
-      >
+      <button className="flex items-center rounded-md border border-gray-300 bg-white py-2 pl-3 pr-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50">
         {capitalize(props.view)} View
         <ChevronDownIcon className="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
       </button>
@@ -34,26 +30,17 @@ interface ViewProps {
 
 type SetState<A> = React.Dispatch<React.SetStateAction<A>>;
 
-interface MenuProps extends ViewProps {
-  close: () => void;
-}
-
-const CalendarViewMenu = (props: MenuProps) => {
-  const handleItemClick = (view: CalendarView) => {
-    props.setView(view);
-    props.close();
-  };
-
+const CalendarViewMenu = (props: ViewProps) => {
   return (
     <Menu
-      className="focus:outline-none right-0 mt-3 w-36 origin-top-right overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5"
+      className={`${Classes.POPOVER2_DISMISS} focus:outline-none right-0 mt-3 w-36 origin-top-right overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5`}
       style={{ minWidth: 0, padding: "4px 0", borderRadius: 8 }}
     >
       {views.map((view) => (
         <li key={view}>
           <button
             className="block px-4 py-2 text-sm text-gray-700 w-full text-left hover:bg-gray-100"
-            onClick={() => handleItemClick(view)}
+            onClick={() => props.setView(view)}
           >
             {capitalize(view)} View
           </button>
