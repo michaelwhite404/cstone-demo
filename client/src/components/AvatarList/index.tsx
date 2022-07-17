6b27d7ce39c0e55ffe22232956f0c090f16bd766
Michael White <michaelwhite404@gmail.com>
2022-07-17 13:08:49 -0400

    Added tooltoip arrow

--- client/src/components/AvatarList/index.tsx
+++ client/src/components/AvatarList/index.tsx
@@ -27,8 +27,15 @@ export default function AvatarList(props: AvatarListProps) {
               alt={user.name}
             />
             {tooltip && (
-              <div className="avatar-tooltip absolute z-30 -top-6 -left-10 bg-gray-400 text-white rounded px-3 whitespace-nowrap">
-                <span>{user.name}</span>
+              <div
+                className="avatar-tooltip absolute z-30 -top-6 -left-10 bg-gray-400 text-white rounded px-3 min-w-max left-1/2"
+                style={{ transform: "translate(-50%, 0)" }}
+              >
+                <div
+                  className="bg-gray-400 w-2 h-2 -bottom-1 absolute left-1/2 z-10"
+                  style={{ transform: "translate(-50%, 0) rotateZ(135deg)" }}
+                />
+                <span className="relative z-20 block">{user.name}</span>
               </div>
             )}
           </div>
