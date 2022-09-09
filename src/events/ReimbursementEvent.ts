9f378a1a0582770d7b0cbd920933c36b10cb86d7
Michael White <michaelwhite404@gmail.com>
2022-09-09 13:00:26 -0400

    Send finalize event to submitting user

--- src/events/ReimbursementEvent.ts
+++ src/events/ReimbursementEvent.ts
@@ -1,5 +1,5 @@
 import { ReimbursementDocument } from "@@types/models";
-import { Employee, Reimbursement } from "@models";
+import { Reimbursement } from "@models";
 import { io } from "@server";
 import { chat } from "@utils";
 import { format } from "date-fns";
@@ -11,12 +11,14 @@ const URL =
 
 class ReimbursementEvent {
   async submit(reimbursement: ReimbursementDocument) {
-    reimbursement = await Reimbursement.populate(reimbursement, { path: "user" });
-    const employee = await Employee.findById(reimbursement.sendTo);
-    if (!employee) return;
-    if (employee.space) {
+    reimbursement = await Reimbursement.populate(reimbursement, "user sendTo");
+    // const employee = await Employee.findById(reimbursement.sendTo);
+    // if (!employee) return;
+
+    if (reimbursement.sendTo._id.toString() === reimbursement.user._id.toString()) return;
+    if (reimbursement.sendTo.space) {
       const response = await chat.spaces.messages.create({
-        parent: employee.space,
+        parent: reimbursement.sendTo.space,
         requestBody: {
           text: `${reimbursement.user.fullName} created a reimbursement request.`,
           cards: [
@@ -118,79 +120,92 @@ class ReimbursementEvent {
     reimbursement = (await Reimbursement.findById(reimbursement)
       .populate("user sendTo")
       .select("+message"))!;
+    if (!reimbursement) return;
     const sendingEmails: string[] = [reimbursement.sendTo.email, reimbursement.user.email];
     const sockets = await io.fetchSockets();
     const sendSockets = sockets.filter((socket) => sendingEmails.includes(socket.data.user?.email));
-    console.log(sendSockets);
     sendSockets.forEach((socket) => io.to(socket.id).emit("finalizeReimbursement", reimbursement));
-    /****** */
-    if (!reimbursement || !reimbursement.message) return;
+    //Create card
+    const card = {
+      header: {
+        title: "Reimbursement Request",
+        subtitle: reimbursement.purpose,
+        imageUrl: "https://i.ibb.co/qMHwLwK/Reimbursement-Blue.png",
+      },
+      sections: [
+        {
+          widgets: [
+            {
+              keyValue: {
+                topLabel: "Payee Name",
+                content: reimbursement.payee,
+              },
+            },
+            {
+              keyValue: {
+                topLabel: "Purchase Date",
+                content: format(new Date(reimbursement.date), "P"),
+              },
+            },
+            {
+              keyValue: {
+                topLabel: "Amount",
+                content: (reimbursement.amount / 100).toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                }),
+              },
+            },
+            {
+              keyValue: {
+                topLabel: "Status",
+                content: reimbursement.approval!.approved
+                  ? '<font color="#00A602">Approved</font>'
+                  : '<font color="#DC143C">Rejected</font>',
+              },
+            },
+          ],
+        },
+        {
+          widgets: [
+            {
+              buttons: [
+                {
+                  textButton: {
+                    text: "OPEN IN APP",
+                    onClick: {
+                      openLink: {
+                        url: `${URL}/requests/reimbursements#${reimbursement._id}`,
+                      },
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+    // Create message for submitting user
+    const sameUser = reimbursement.sendTo._id.toString() === reimbursement.user._id.toString();
+    if (reimbursement.user.space && !sameUser) {
+      await chat.spaces.messages.create({
+        parent: reimbursement.user.space,
+        requestBody: {
+          text: `Your reimbursement request for '${reimbursement.purpose}' has been ${
+            reimbursement.approval!.approved ? "Approved" : "Rejected"
+          }`,
+          cards: [card],
+        },
+      });
+    }
+    // Updaee message to "sendTo" user
+    if (!reimbursement.message) return;
     await chat.spaces.messages.update({
       name: reimbursement.message,
       updateMask: "cards",
       requestBody: {
-        cards: [
-          {
-            header: {
-              title: "Reimbursement Request",
-              subtitle: reimbursement.purpose,
-              imageUrl: "https://i.ibb.co/qMHwLwK/Reimbursement-Blue.png",
-            },
-            sections: [
-              {
-                widgets: [
-                  {
-                    keyValue: {
-                      topLabel: "Payee Name",
-                      content: reimbursement.payee,
-                    },
-                  },
-                  {
-                    keyValue: {
-                      topLabel: "Purchase Date",
-                      content: format(new Date(reimbursement.date), "P"),
-                    },
-                  },
-                  {
-                    keyValue: {
-                      topLabel: "Amount",
-                      content: (reimbursement.amount / 100).toLocaleString("en-US", {
-                        style: "currency",
-                        currency: "USD",
-                      }),
-                    },
-                  },
-                  {
-                    keyValue: {
-                      topLabel: "Status",
-                      content: reimbursement.approval!.approved
-                        ? '<font color="#00A602">Approved</font>'
-                        : '<font color="#DC143C">Rejected</font>',
-                    },
-                  },
-                ],
-              },
-              {
-                widgets: [
-                  {
-                    buttons: [
-                      {
-                        textButton: {
-                          text: "OPEN IN APP",
-                          onClick: {
-                            openLink: {
-                              url: `${URL}/requests/reimbursements#${reimbursement._id}`,
-                            },
-                          },
-                        },
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
-          },
-        ],
+        cards: [card],
       },
     });
   }
