d86d16e138c7b6442f58a6e2f8751e63900ecaf2
Michael White <michaelwhite404@gmail.com>
2022-09-15 23:33:09 -0400

    Tickets Empty State

---
+++ client/src/components/EmptyStateIllustration.tsx
@@ -0,0 +1,21 @@
+export default function EmptyStateIllustration(props: EmptyStateIllustrationProps) {
+  const { imgSrc, text, xlWidth } = props;
+  return (
+    <div className="flex justify-center w-full lg:absolute">
+      <div className="flex align-center justify-center flex-col mt-8 py-5">
+        <img
+          className={`w-[80%] lg:w-[70%] xl:w-[${xlWidth}] opacity-70`}
+          src={imgSrc}
+          alt="Sick Leave"
+        />
+        <div className="lg:text-lg text-md font-medium text-center mt-2 text-gray-500">{text}</div>
+      </div>
+    </div>
+  );
+}
+
+interface EmptyStateIllustrationProps {
+  imgSrc: string;
+  text: string;
+  xlWidth: string;
+}
--- client/src/pages/Tickets/index.tsx
+++ client/src/pages/Tickets/index.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { TicketModel } from "../../../../src/types/models";
+import EmptyStateIllustration from "../../components/EmptyStateIllustration";
 import PrimaryButton from "../../components/PrimaryButton/PrimaryButton";
 import CreateTicketModal from "./CreateTicketModal";
 import MyTicketTable from "./MyTicketTable";
@@ -9,25 +10,44 @@ import TicketDetails from "./TicketDetails";
 export default function Tickets() {
   const [tickets, setTickets] = useState<TicketModel[]>([]);
   const [modalOpen, setModalOpen] = useState(false);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     const fetchTickets = async () => {
+      setLoaded(false);
       const res = await axios.get("/api/v2/tickets");
       setTickets(res.data.data.tickets);
+      setLoaded(true);
     };
     fetchTickets();
   }, []);
   return (
     <div className="flex flex-col" style={{ padding: "10px 25px 25px" }}>
-      {/* Header */}
-      <div className="page-header">
-        <h1 style={{ marginBottom: "10px" }}>Tickets</h1>
-        {/* <p>Manage the team and account permissions here</p> */}
-        <PrimaryButton text="+ Create New Ticket" onClick={() => setModalOpen(true)} />
+      <div className="sm:flex sm:justify-between align-center">
+        {/* Header */}
+        <div className="page-header">
+          <h1 style={{ marginBottom: "10px" }}>Tickets</h1>
+          {/* <p>Manage the team and account permissions here</p> */}
+        </div>
+        <PrimaryButton
+          className="w-full sm:w-auto"
+          text="+ Create New Ticket"
+          onClick={() => setModalOpen(true)}
+        />
       </div>
-      <div>
-        <MyTicketTable tickets={tickets} />
-      </div>
+      {loaded && (
+        <div className="relative">
+          {tickets.length > 0 ? (
+            <MyTicketTable tickets={tickets} />
+          ) : (
+            <EmptyStateIllustration
+              imgSrc="/Helpdesk_Illustration.png"
+              text="You don't have any tickets so far"
+              xlWidth={"75%"}
+            />
+          )}
+        </div>
+      )}
       <CreateTicketModal open={modalOpen} setOpen={setModalOpen} />
     </div>
   );
