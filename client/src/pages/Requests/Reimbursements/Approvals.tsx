604db3d53a4199b22c772a12aa6cbe90a60c77e3
Michael White <michaelwhite404@gmail.com>
2022-09-17 19:28:29 -0400

    Empty State Aprovals (UI)

--- client/src/pages/Requests/Reimbursements/Approvals.tsx
+++ client/src/pages/Requests/Reimbursements/Approvals.tsx
@@ -5,6 +5,7 @@ import pluralize from "pluralize";
 import { RM } from ".";
 import { EmployeeModel, ReimbursementModel } from "../../../../../src/types/models";
 import ApprovalBadge from "../../../components/Badges/ApprovalBadge";
+import EmptyStateIllustration from "../../../components/EmptyStateIllustration";
 import TableWrapper from "../../../components/TableWrapper";
 import { useChecker2, useToasterContext } from "../../../hooks";
 import ApprovalsList from "./ApprovalsList";
@@ -20,13 +21,8 @@ interface Props {
 
 const headings = ["Purpose", "Date", "Amount", "Submitted By", "Status"];
 
-export default function Approvals({
-  reimbursements,
-  select,
-  user,
-  finalizeReimbursement,
-  setReimbursements,
-}: Props) {
+export default function Approvals(props: Props) {
+  const { reimbursements, select, user, finalizeReimbursement, setReimbursements } = props;
   const { showToaster } = useToasterContext();
   const approvals = reimbursements.filter((r) => r.sendTo?._id === user._id);
   const { pending, finalized } = approvals.reduce(
@@ -63,122 +59,146 @@ export default function Approvals({
   };
 
   return (
-    <div>
-      <div>
-        <div className="mt-4 sm:h-14 sm:flex items-center justify-between">
-          <span className="text-xl font-bold text-gray-900">Pending ({data.length})</span>
-          <div
-            className={`${
-              selectedData.length ? "sm:block flex" : "hidden"
-            } space-x-3  pt-2 sm:pt-0`}
-          >
-            <button
-              type="button"
-              className="w-full sm:w-auto justify-center inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
-              onClick={() => handleMultiFinalize(true)}
-            >
-              <CheckIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
-              Approve {allSelected ? "All" : ""}
-            </button>
-            <button
-              type="button"
-              className="w-full sm:w-auto justify-center inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
-              onClick={() => handleMultiFinalize(false)}
-            >
-              <XIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
-              Reject {allSelected ? "All" : ""}
-            </button>
+    <div className="relative">
+      {reimbursements.length ? (
+        <div>
+          <div>
+            <div className="mt-4 sm:h-14 sm:flex items-center justify-between">
+              <span className="text-xl font-bold text-gray-900">Pending ({data.length})</span>
+              <div
+                className={`${
+                  selectedData.length ? "sm:block flex" : "hidden"
+                } space-x-3  pt-2 sm:pt-0`}
+              >
+                <button
+                  type="button"
+                  className="w-full sm:w-auto justify-center inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                  onClick={() => handleMultiFinalize(true)}
+                >
+                  <CheckIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+                  Approve {allSelected ? "All" : ""}
+                </button>
+                <button
+                  type="button"
+                  className="w-full sm:w-auto justify-center inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                  onClick={() => handleMultiFinalize(false)}
+                >
+                  <XIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+                  Reject {allSelected ? "All" : ""}
+                </button>
+              </div>
+            </div>
+            {pending.length ? (
+              <div>
+                <div className="hidden sm:block">
+                  <TableWrapper>
+                    <table className="table-auto">
+                      <thead>
+                        <tr>
+                          <th scope="col" className="relative w-12 px-6 sm:w-16 sm:px-8 border-b">
+                            <input
+                              type="checkbox"
+                              className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 sm:left-6"
+                              ref={checkboxRef}
+                              checked={allSelected && reimbursements.length > 0}
+                              onChange={toggleAll}
+                            />
+                          </th>
+                          {headings.map((h) => (
+                            <th key={h}>{h}</th>
+                          ))}
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {data.map((reimbursement) => {
+                          const selected = selectedData.includes(reimbursement);
+                          return (
+                            <tr
+                              className={classNames(
+                                { "bg-indigo-50": selected },
+                                { "hover:bg-gray-100": !selected }
+                              )}
+                              key={reimbursement._id}
+                            >
+                              <td className="relative w-12 px-6 sm:w-16 sm:px-8 pl-4 py-2.5 border-b border-gray-300">
+                                <input
+                                  type="checkbox"
+                                  className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 sm:left-6"
+                                  value={reimbursement._id}
+                                  checked={selected}
+                                  onChange={(e) =>
+                                    setSelectedData(
+                                      e.target.checked
+                                        ? [...selectedData, reimbursement]
+                                        : selectedData.filter((r) => r !== reimbursement)
+                                    )
+                                  }
+                                />
+                              </td>
+
+                              <td
+                                onClick={() => select(reimbursement)}
+                                className="py-2.5 border-b border-gray-300 font-medium text-blue-500 cursor-pointer"
+                              >
+                                {reimbursement.purpose}
+                              </td>
+                              <td className="py-2.5 border-b border-gray-300">
+                                {format(new Date(reimbursement.date), "P")}
+                              </td>
+                              <td className="py-2.5  border-b border-gray-300 text-gray-400 ">
+                                {(reimbursement.amount / 100).toLocaleString("en-US", {
+                                  style: "currency",
+                                  currency: "USD",
+                                })}
+                              </td>
+                              <td className="py-2.5 border-b border-gray-300 text-gray-400">
+                                {reimbursement.user.fullName}
+                              </td>
+                              <td className="py-2.5 border-b border-gray-300 text-gray-400">
+                                <ApprovalBadge status={reimbursement.status} />
+                              </td>
+                            </tr>
+                          );
+                        })}
+                      </tbody>
+                    </table>
+                  </TableWrapper>
+                </div>
+                <div className="block sm:hidden">
+                  <ApprovalsList reimbursements={pending} select={select} />
+                </div>
+              </div>
+            ) : (
+              <span className="ml-5">There are no pending reimbursements right now</span>
+            )}
+          </div>
+          <div>
+            <div className="mt-4 sm:h-14 sm:flex items-center justify-between">
+              <span className="text-xl font-bold text-gray-900">
+                Finalized ({finalized.length})
+              </span>
+            </div>
+            {finalized.length ? (
+              <div>
+                <div className="hidden sm:block">
+                  <ApprovalsTable reimbursements={finalized} select={select} />
+                </div>
+                <div className="block sm:hidden">
+                  <ApprovalsList reimbursements={finalized} select={select} />
+                </div>
+              </div>
+            ) : (
+              <span className="ml-5">There are no finalized reimbursements right now</span>
+            )}
           </div>
         </div>
-        <div className="hidden sm:block">
-          <TableWrapper>
-            <table className="table-auto">
-              <thead>
-                <tr>
-                  <th scope="col" className="relative w-12 px-6 sm:w-16 sm:px-8 border-b">
-                    <input
-                      type="checkbox"
-                      className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 sm:left-6"
-                      ref={checkboxRef}
-                      checked={allSelected && reimbursements.length > 0}
-                      onChange={toggleAll}
-                    />
-                  </th>
-                  {headings.map((h) => (
-                    <th key={h}>{h}</th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {data.map((reimbursement) => {
-                  const selected = selectedData.includes(reimbursement);
-                  return (
-                    <tr
-                      className={classNames(
-                        { "bg-indigo-50": selected },
-                        { "hover:bg-gray-100": !selected }
-                      )}
-                      key={reimbursement._id}
-                    >
-                      <td className="relative w-12 px-6 sm:w-16 sm:px-8 pl-4 py-2.5 border-b border-gray-300">
-                        <input
-                          type="checkbox"
-                          className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 sm:left-6"
-                          value={reimbursement._id}
-                          checked={selected}
-                          onChange={(e) =>
-                            setSelectedData(
-                              e.target.checked
-                                ? [...selectedData, reimbursement]
-                                : selectedData.filter((r) => r !== reimbursement)
-                            )
-                          }
-                        />
-                      </td>
-
-                      <td
-                        onClick={() => select(reimbursement)}
-                        className="py-2.5 border-b border-gray-300 font-medium text-blue-500 cursor-pointer"
-                      >
-                        {reimbursement.purpose}
-                      </td>
-                      <td className="py-2.5 border-b border-gray-300">
-                        {format(new Date(reimbursement.date), "P")}
-                      </td>
-                      <td className="py-2.5  border-b border-gray-300 text-gray-400 ">
-                        {(reimbursement.amount / 100).toLocaleString("en-US", {
-                          style: "currency",
-                          currency: "USD",
-                        })}
-                      </td>
-                      <td className="py-2.5 border-b border-gray-300 text-gray-400">
-                        {reimbursement.user.fullName}
-                      </td>
-                      <td className="py-2.5 border-b border-gray-300 text-gray-400">
-                        <ApprovalBadge status={reimbursement.status} />
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </TableWrapper>
-        </div>
-        <div className="block sm:hidden">
-          <ApprovalsList reimbursements={pending} select={select} />
-        </div>
-      </div>
-      <div>
-        <div className="mt-4 sm:h-14 sm:flex items-center justify-between">
-          <span className="text-xl font-bold text-gray-900">Finalized ({finalized.length})</span>
-        </div>
-        <div className="hidden sm:block">
-          <ApprovalsTable reimbursements={finalized} select={select} />
-        </div>
-        <div className="block sm:hidden">
-          <ApprovalsList reimbursements={finalized} select={select} />
-        </div>
-      </div>
+      ) : (
+        <EmptyStateIllustration
+          text="There are no reimbursement approvals so far"
+          imgSrc="https://cdni.iconscout.com/illustration/premium/thumb/man-endorses-document-with-an-ink-pen-2549497-2145099.png"
+          xlWidth="100%"
+        />
+      )}
     </div>
   );
 }
