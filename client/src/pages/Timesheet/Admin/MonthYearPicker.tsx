9d2114202deed325ecde277dc360324ae291ff6a
Michael White <michaelwhite404@gmail.com>
2022-09-25 17:40:56 -0400

    Remove fetch on year change

--- client/src/pages/Timesheet/Admin/MonthYearPicker.tsx
+++ client/src/pages/Timesheet/Admin/MonthYearPicker.tsx
@@ -1,8 +1,9 @@
 import { Menu, Transition } from "@headlessui/react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 import { ChevronDownIcon } from "@heroicons/react/solid";
-import { add, format } from "date-fns";
-import React, { Fragment } from "react";
+import classNames from "classnames";
+import { format, isFuture } from "date-fns";
+import React, { Fragment, useMemo, useState } from "react";
 
 const months = [
   "January",
@@ -21,7 +22,10 @@ const months = [
 
 export default function MonthYearPicker(props: Props) {
   const { date, setDate } = props;
-  const changeYear = (change: number) => setDate(add(date, { years: change }));
+  const [year, setYear] = useState(date.getFullYear());
+  const changeYear = (change: number) => setYear((y) => y + change);
+
+  const newYearDisabled = useMemo(() => isFuture(new Date(year + 1, 1)), [year]);
 
   return (
     <Menu as="div" className="relative">
@@ -47,20 +51,31 @@ export default function MonthYearPicker(props: Props) {
               <button className="p-1.5 shadow rounded-lg" onClick={() => changeYear(-1)}>
                 <ChevronLeftIcon className="w-5 h-5" />
               </button>
-              <div className="flex align-center font-medium">{props.date.getFullYear()}</div>
-              <button className="p-1.5 shadow rounded-lg" onClick={() => changeYear(1)}>
-                <ChevronRightIcon className="w-5 h-5" />
+              <div className="flex align-center font-medium">{year}</div>
+              <button
+                disabled={newYearDisabled}
+                className="p-1.5 shadow rounded-lg"
+                onClick={() => changeYear(1)}
+              >
+                <ChevronRightIcon
+                  className={classNames("w-5 h-5", { "text-gray-300": newYearDisabled })}
+                />
               </button>
             </div>
             <div className="grid grid-cols-3 grid-rows-4 gap-2">
               {months.map((month, i) => (
-                <Menu.Item key={i}>
-                  {({ active }) => (
+                <Menu.Item key={i} disabled={isFuture(new Date(`${month} 1, ${year}`))}>
+                  {({ disabled }) => (
                     <button
-                      className="p-2 font-medium focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded"
-                      onClick={() =>
-                        props.setDate(new Date(`${month} 1, ${props.date.getFullYear()}`))
-                      }
+                      className={classNames(
+                        "p-2 font-medium rounded",
+                        {
+                          "focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500":
+                            !disabled,
+                        },
+                        { "text-gray-300 cursor-default": disabled }
+                      )}
+                      onClick={() => setDate(new Date(`${month} 1, ${year}`))}
                     >
                       {month}
                     </button>
